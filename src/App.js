76de2dda39beba6f0bf3cd6af800cc188d7c2b7f
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-12 15:28:32 +0500

    understanding the concept of useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import NavBar from "./component/NavBar";
+import { useState } from "react";
 function App() {
+  const [name, setName] = useState("ikram");
+
+  const handleOnClick = () => {
+    setName("Ali");
+  }
   return (
     <>
       <NavBar />
+      <div className="container-fluid">
+        <div className="row">
+          <p>{name}</p>
+          <button type="button" class="btn btn-danger" onClick={handleOnClick}>Click Me</button>
+        </div>
+      </div>
     </>
   );
 }

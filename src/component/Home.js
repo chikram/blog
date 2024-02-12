6bd5a9229ea9307a4c80c2ffd2c3677064ab5c33
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-12 23:46:32 +0500

    The use of filter method

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,17 +3,17 @@ import BlogList from "./BlogList";
 import NavBar from "./NavBar"
 function Home() {
     const [blogs, setBlogs] = useState([
-        { title: "My new website", author: "Ali", id: 1 },
-        { title: "Welcome Party", author: "Ali", id: 2 },
-        { title: "Web dev top tips", author: "Ali", id: 3 },
+        { title: "My new website", author: "Ikram", id: 1 },
+        { title: "Welcome Party", author: "Saeed", id: 2 },
+        { title: "Web dev top tips", author: "Ikram", id: 3 },
     ]);
     return (
         <>
             <div className="container mx-30">
                 <NavBar />
             </div>
-
             <BlogList prob={blogs} title="All Blogs!" />
+            <BlogList prob={blogs.filter((blog) => blog.author === 'Ikram')} title="Ikram Blogs!" />
         </>
     )
 }

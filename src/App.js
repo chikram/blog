ee25fd93f498178e239ede399ed530174d21cc35
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-12 15:04:50 +0500

    modifying nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import NavBar from "./component/NavBar";
 function App() {
   return (
-    <div className="container-fluid">
+    <>
       <NavBar />
-    </div>
+    </>
   );
 }
 
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -4,17 +4,17 @@ function NavBar() {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Blog</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className="navbar-brand text-danger fw-bold" href="/">My Blog</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <a className="nav-link fw-bold" aria-current="page" href="/">Home</a>
+                                <a className="nav-link text-dark" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link fw-bold" href="/">Link</a>
+                                <a className="nav-link text-dark" aria-current="page" href="/">New Blog</a>
                             </li>
                         </ul>
                     </div>

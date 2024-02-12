f358a31eeb04a38c6a02b0a0c8dd148b44b92dbc
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-12 16:32:27 +0500

    Map function to render an array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import NavBar from "./component/NavBar";
 import { useState } from "react";
 function App() {
-  const [name, setName] = useState("ikram");
-
-  const handleOnClick = () => {
-    setName("Ali");
-  }
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", author: "Ali", id: 1 },
+    { title: "Welcome Party", author: "Ali", id: 2 },
+    { title: "Web dev top tips", author: "Ali", id: 3 }
+  ]);
   return (
     <>
       <NavBar />
-      <div className="container-fluid">
-        <div className="row">
-          <p>{name}</p>
-          <button type="button" class="btn btn-danger" onClick={handleOnClick}>Click Me</button>
-        </div>
+      <div className="container ">
+
+        {blogs.map((blog) => (
+          <div className="row" key={blog.id}>
+            <div className="col-lg-6 col-sm-10 col-10 mx-auto">
+              <h2 className="text-danger">{blog.title}</h2>
+              <p>{blog.title}</p>
+            </div>
+          </div>
+
+        ))}
       </div>
     </>
   );

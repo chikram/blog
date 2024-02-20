1bd79d01c08a70b3a25b4f3a958308ddbfcee720
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-21 02:33:45 +0500

    making costom hooks

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,33 +1,8 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 import NavBar from "./NavBar"
+import useFetch from "./useFetch";
 function Home() {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('could not fatch the data from that resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsPending(false)
-                    setError(null)
-                })
-                .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
-                })
-        }, 1000);
-
-    }, [])
-
-
+    const { blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
     return (
         <>
             <div className="container mx-30">
---
+++ src/component/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+const useFetch = (url) => {
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fatch the data from that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false)
+                    setError(null)
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
+        }, 1000);
+
+    }, [])
+    return { blogs, isPending, error };
+}
+
+export default useFetch;

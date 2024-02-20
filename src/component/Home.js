3eefca59d803a182b18153e520f60cbe95943a91
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-21 01:01:54 +0500

    conditional Loading message

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,14 +3,18 @@ import BlogList from "./BlogList";
 import NavBar from "./NavBar"
 function Home() {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true)
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
-            });
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false)
+                });
+        }, 1000);
 
     }, [])
 
@@ -20,7 +24,14 @@ function Home() {
             <div className="container mx-30">
                 <NavBar />
             </div>
-            {blogs && <BlogList prob={blogs} title="Ali Blogs!" />}
+            <div className="py-5">
+                {isPending && <div className="text-center">
+                    <div className="spinner-border" role="status">
+                        <span className="sr-only"></span>
+                    </div>
+                </div>}
+                {blogs && <BlogList prob={blogs} title="Ali Blogs!" />}
+            </div>
 
         </>
 

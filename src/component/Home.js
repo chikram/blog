026276ccf7ed0e8c27523fc2d6448bfad0cfd855
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-21 02:00:09 +0500

    handling fetch error

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,16 +4,25 @@ import NavBar from "./NavBar"
 function Home() {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fatch the data from that resource');
+                    }
                     return res.json();
                 })
                 .then(data => {
                     setBlogs(data);
                     setIsPending(false)
-                });
+                    setError(null)
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
         }, 1000);
 
     }, [])
@@ -25,6 +34,7 @@ function Home() {
                 <NavBar />
             </div>
             <div className="py-5">
+                {error && <div className=" text-center p-2 mb-1 bg-warning text-dark">{error}</div>}
                 {isPending && <div className="text-center">
                     <div className="spinner-border" role="status">
                         <span className="sr-only"></span>

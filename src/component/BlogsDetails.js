ffd1c684a6f8e8858fcf4953d551856e5fd590da
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-24 00:38:29 +0500

    reusing custom hook

--- src/component/BlogsDetails.js
+++ src/component/BlogsDetails.js
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogsDetails = () => {
     const { id } = useParams()
+    const { blogs, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
     return (
-        <div className="container py-3">
-            <h2 className="text-center">Detail of the Blog- {id}</h2>
+        <div classNameName="container py-5">
+            {error && <div classNameName=" text-center p-2 mb-1 bg-warning text-dark">{error}</div>}
+            {isPending && <div className="text-center m-2">
+                <div className="spinner-border" role="status">
+                    <span className="sr-only"></span>
+                </div>
+            </div>}
+            {blogs && (<div className="card mx-auto m-5" style={{ width: "80%" }}>
+                <img className="card-img-top" src={blogs.img} alt="Cardcap" />
+                <div className="card-body">
+                    <h4 className="card-title text-center">{blogs.title}</h4>
+                    <p className="card-text">{blogs.body}</p>
+                </div>
+            </div>)}
         </div>
     )
 }
--- src/component/Create.js
+++ src/component/Create.js
@@ -3,43 +3,43 @@ const Create = () => {
         <>
             <div className="container py-3">
                 <div className="row ">
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-8">
                         <p className="text-center">Ronaldo</p>
                         <img src="r3.jpg" alt="" width={350} />
                     </div>
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-8">
                         <p className="text-center">Ronaldo</p>
                         <img src="r3.jpg" alt="" width={350} />
                     </div>
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-8">
                         <p className="text-center">Alexandra</p>
                         <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
                     </div>
                 </div>
                 <div className="row ">
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-4">
                         <p className="text-center">Alexandra</p>
                         <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
                     </div>
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-4">
                         <p className="text-center">helllo</p>
                         <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
                     </div>
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-4">
                         <p className="text-center">helllo</p>
                         <img src="r3.jpg" alt="" width={350} />
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-4">
                         <p className="text-center">Alexandra</p>
                         <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
                     </div>
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-4">
                         <p className="text-center">helllo</p>
                         <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
                     </div>
-                    <div className="col col-md-4 col-sm-6">
+                    <div className="col col-md-4 col-sm-4">
                         <p className="text-center">helllo</p>
                         <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
                     </div>

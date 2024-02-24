2891c443649d433826596d047ec426eea88530d6
Ikram Aslam <ikramaslam1020@gmail.com>
2024-02-24 21:46:51 +0500

    controlled inputs(forms)

--- src/component/Create.js
+++ src/component/Create.js
@@ -1,53 +1,50 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [author, setAuthor] = useState('Ikram')
     return (
         <>
-            <div className="container py-3">
-                <div className="row ">
-                    <div className="col col-md-4 col-sm-8">
-                        <p className="text-center">Ronaldo</p>
-                        <img src="r3.jpg" alt="" width={350} />
+            <div classNameName="container py-3">
+                <h1 className="text-danger py-2 text-center">Add a New Blog</h1>
+                <form className="py-2">
+                    <div className="mb-2">
+                        <label for="text1" className="form-label">
+                            Blog Title
+                        </label>
+                        <input type="text" className="form-control" id="text1" required value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
                     </div>
-                    <div className="col col-md-4 col-sm-8">
-                        <p className="text-center">Ronaldo</p>
-                        <img src="r3.jpg" alt="" width={350} />
+                    <div className="mb-3">
+                        <label for="floatingTextarea">Blog Body</label>
+                        <textarea
+                            className="form-control"
+                            placeholder="Write something"
+                            id="floatingTextarea"
+                            value={body}
+                            onChange={(e) => setBody(e.target.value)}
+                        ></textarea>
                     </div>
-                    <div className="col col-md-4 col-sm-8">
-                        <p className="text-center">Alexandra</p>
-                        <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
+                    <div className=" dropdown my-3">
+                        <label for="dropdownMenuButton1">Blog Author</label> <br />
+                        <select className="drp" value={author}
+                            onChange={(e) => setAuthor(e.target.value)}
+                        >
+                            <option value="Ikram">Ikram</option>
+                            <option value="Saeed">Saeed</option>
+                        </select>
                     </div>
-                </div>
-                <div className="row ">
-                    <div className="col col-md-4 col-sm-4">
-                        <p className="text-center">Alexandra</p>
-                        <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
+                    <div class="col-md-12 text-center">
+                        <button type="button" class="btn btn-danger">
+                            Add Blog
+                        </button>
                     </div>
-                    <div className="col col-md-4 col-sm-4">
-                        <p className="text-center">helllo</p>
-                        <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
-                    </div>
-                    <div className="col col-md-4 col-sm-4">
-                        <p className="text-center">helllo</p>
-                        <img src="r3.jpg" alt="" width={350} />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col col-md-4 col-sm-4">
-                        <p className="text-center">Alexandra</p>
-                        <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
-                    </div>
-                    <div className="col col-md-4 col-sm-4">
-                        <p className="text-center">helllo</p>
-                        <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
-                    </div>
-                    <div className="col col-md-4 col-sm-4">
-                        <p className="text-center">helllo</p>
-                        <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/0*ShPUZvTVCt52vNbJ.jpg" alt="" width={350} />
-                    </div>
-                </div>
-
+                </form>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Create;
